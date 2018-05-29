bf1d31b1d6eda6ab32f201162d4565736d1542fa
Aureo Freitas <aureo.carneiro@gmail.com>
2018-05-29 09:58:47 -0300

    Imp. generator Cod

--- database/bibo-dbgenerator.py
+++ database/bibo-dbgenerator.py
@@ -10,31 +10,31 @@ tag = str(sys.argv[4])
 scan = str(sys.argv[5])
 
 try:
+    file = open(arquivo, 'r')
+    conteudo = file.readlines()
+
     if tipo == "bi":
-        file = open(arquivo,'r')
-        conteudo = file.readlines()
         conteudo.append('\n' + 'record (bi, "$(IOC):{}")'.format(pv) + '\n' + '{' + '\n' \
-                        + '    field (INP, "@$(PLC) {}")'.format(tag) + '\n'
-                        + '    field (SCAN, "{} second")'.format(scan) + '\n'
+                        + '    field(INP, "@$(PLC) {}")'.format(tag) + '\n' \
+                        + '    field(SCAN, "{} second")'.format(scan) + '\n' \
+                        + '    field(ZNAM, "False")' + '\n' \
+                        + '    field(ONAM, "True")' + '\n' \
+                        + '    field(DTYP, "EtherIP")' + '\n' \
                         + '}' + '\n')
-        file = open(arquivo, 'w')
-        file.writelines(conteudo)
-        file.close()
 
     elif tipo == "bo":
-        file = open(arquivo,'r')
-        conteudo = file.readlines()
         conteudo.append('\n' + 'record (bo, "$(IOC):{}")'.format(pv) + '\n' + '{' + '\n' \
                         + '    field(SCAN, "Passive")'+ '\n' \
                         + '    field(DTYP, "EtherIP")'+ '\n' \
                         + '    field(OUT, "@$(PLC) {}")'.format(tag) + '\n' \
                         + '    field(ZNAM, "False")' + '\n' \
-                        + '    field(ONAM, "True")' + '\n'
-                        + '    field (SCAN, "{} second")'.format(scan) + '\n'
+                        + '    field(ONAM, "True")' + '\n' \
+                        + '    field(SCAN, "{} second")'.format(scan) + '\n' \
                         + '}' + '\n')
-        file = open(arquivo, 'w')
-        file.writelines(conteudo)
-        file.close()
+
+    file = open(arquivo, 'w')
+    file.writelines(conteudo)
+    file.close()
 
 except Exception as e:
     print(e)
