b4daae18d099f11caaddc232cefb8bea5b898bbe
carneirofc <claudiofcarneiro@hotmail.com>
2019-06-14 12:50:27 -0300

    script: IOC generation update

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,12 @@
 
 # Generate Sirius RF Booster IOC
 ./ioc.py \
-    ../etc/Interlock_RF_EPICS_V2.xlsx \
-    --plc-ip 10.0.28.135 \
+    ../etc/BOIlk \
+    --plc-ip 10.128.124.150 \
     --plc-name plc1 \
     --plc-module 0 \
     --ioc-name RF-Booster \
-    --sheet Booster \
-    --epics-ca-server-port 5100 \
-    --epics-cas-intf-addr-list 10.0.6.57 \
+    --sheet TUDO\
+    --epics-ca-server-port  5064\
+    --epics-cas-intf-addr-list 10.128.124.204 \
     --arch linux-x86_64
