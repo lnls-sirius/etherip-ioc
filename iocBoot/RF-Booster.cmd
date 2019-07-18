95ca8fb94a809e06f2a3a0ea702ccb966cf4e11e
carneirofc <claudiofcarneiro@hotmail.com>
2019-07-18 12:11:29 -0300

    BO-05D:RF-P5Cav:Sts-Mon

--- iocBoot/RF-Booster.cmd
+++ iocBoot/RF-Booster.cmd
@@ -1,4 +1,5 @@
 #!/opt/epics-R3.15.5/modules/ether_ip-ether_ip-3-1/bin/linux-x86_64/eipIoc
+# 3.14 example startup file for a Host - * - shell-script - * -
 
 # Load dbd, register the drvEtherIP .. commands
 dbLoadDatabase("/opt/epics-R3.15.5/modules/ether_ip-ether_ip-3-1/dbd/eipIoc.dbd")
