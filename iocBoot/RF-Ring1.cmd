37b73e1fa03a400432ed859a36cc54022e50eb9d
carneirofc <claudiofcarneiro@hotmail.com>
2019-08-30 11:26:48 -0300

    SI: PVs and service

--- iocBoot/RF-Ring1.cmd
+++ iocBoot/RF-Ring1.cmd
@@ -7,7 +7,7 @@ eipIoc_registerRecordDeviceDriver(pdbbase)
 # epicsEnvSet("EPICS_IOC_LOG_INET", "127.0.0.1")
 # epicsEnvSet("EPICS_IOC_LOG_PORT", "6505")
 
-epicsEnvSet("EPICS_CA_SERVER_PORT", "5068")
+#epicsEnvSet("EPICS_CA_SERVER_PORT", "5068")
 epicsEnvSet("EPICS_CAS_INTF_ADDR_LIST", "10.128.124.141")
 iocLogInit
 
@@ -15,7 +15,7 @@ iocLogInit
 EIP_buffer_limit(450)
 drvEtherIP_init()
 EIP_verbosity(7)
-drvEtherIP_define_PLC("plc1", "10.128.130.151", 0)
+drvEtherIP_define_PLC("plc1", "10.128.130.60", 0)
 
 dbLoadRecords("../database/RF-Ring1.db", "PLC=plc1")
 dbLoadRecords("../database/RF-Ring1-Calc.db", "PLC=plc1")
--- scripts/si-1.sh
+++ scripts/si-1.sh
@@ -6,7 +6,7 @@
     --epics-ca-server-port  5068\
     --epics-cas-intf-addr-list 10.128.124.141 \
     --sheet 'SSAmp Tower 01,SSAmp Tower 02,Interlock,Petra 7,LLRF,Transmission Line' \
-    --plc-ip 10.128.130.151 \
+    --plc-ip 10.128.130.60 \
     --plc-name plc1 \
     --plc-module 0 \
     --ioc-name RF-Ring1 \
