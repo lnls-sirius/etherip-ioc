7dc0a7a0f140fc4f4f3cb7579962a34c20c90083
carneirofc <claudiofcarneiro@hotmail.com>
2020-06-10 10:13:51 -0300

    Testing data

--- iocBoot/SiriusTest.cmd
+++ iocBoot/SiriusTest.cmd
@@ -8,16 +8,15 @@ eipIoc_registerRecordDeviceDriver(pdbbase)
 # epicsEnvSet("EPICS_IOC_LOG_PORT", "6505")
 
 #epicsEnvSet("EPICS_CA_SERVER_PORT", "5064")
-epicsEnvSet("EPICS_CAS_INTF_ADDR_LIST", "10.128.124.140")
+#epicsEnvSet("EPICS_CAS_INTF_ADDR_LIST", "10.128.124.140")
 iocLogInit
 
 # Initialize EtherIP driver, define PLCs
 EIP_buffer_limit(450)
 drvEtherIP_init()
 EIP_verbosity(7)
-drvEtherIP_define_PLC("plc1", "10.128.130.150", 0)
+drvEtherIP_define_PLC("plc1", "10.0.38.199", 0)
 
-dbLoadRecords("../database/RF-Booster.db", "PLC=plc1")
-dbLoadRecords("../database/RF-Booster-Calc.db", "PLC=plc1")
+dbLoadRecords("../database/SiriusTest.db", "PLC=plc1")
 iocInit()
 
