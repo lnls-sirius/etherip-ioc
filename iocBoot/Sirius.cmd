bb3a00d87fe0d5998c2676e1874a4091dccc449b
carneirofc <claudiofcarneiro@hotmail.com>
2020-06-10 11:10:31 -0300

    Initial Sirius pv set

--- iocBoot/Sirius.cmd
+++ iocBoot/Sirius.cmd
@@ -17,6 +17,6 @@ drvEtherIP_init()
 EIP_verbosity(7)
 drvEtherIP_define_PLC("plc1", "10.0.38.199", 1)
 
-dbLoadRecords("../database/SiriusTest.db", "PLC=plc1")
+dbLoadRecords("../database/Sirius.db", "PLC=plc1")
 iocInit()
 
--- scripts/sirius.py
+++ scripts/sirius.py
@@ -1,5 +1,7 @@
-#!/usr/bin/env
+#!/usr/bin/env python
+import os
 from string import Template
+
 bi_template = Template(
     """
 record(bi, "${pv}"){
@@ -7,15 +9,55 @@ record(bi, "${pv}"){
     field(INP, "@$(PLC) ${tag}")
     field(DESC, "${desc}")
     field(SCAN, "${scan} second")
-    field(ONAM, "${highname}")
-    field(ZNAM, "${lowname}")
+    field(ONAM, "${onam}")
+    field(ZNAM, "${znam}")
 }
 """
 )
-
-data = [{
-        "tag":"Program:IHM.EXT_BO_01D_MA_B.Alimentacao",
-}]
+default = {"onam": "Enable", "znam": "Disable", "desc": ""}
+data = [
+    {
+        "tag": "Program:IHM.EXT_BO_01D_MA_B.Input_Temperatura",
+        "scan": ".1",
+        "template": bi_template,
+        "name": "IHM:EXT_BO_01D_MA_B.Input_Temperatura",
+    },
+    {
+        "tag": "Program:IHM.EXT_BO_01D_MA_B.Input_Protecao",
+        "scan": ".1",
+        "template": bi_template,
+        "name": "IHM:EXT_BO_01D_MA_B.Input_Protecao",
+    },
+    {
+        "tag": "Program:IHM.EXT_BO_01D_MA_B.Input_Protecao",
+        "scan": ".1",
+        "template": bi_template,
+        "name": "IHM:EXT_BO_01D_MA_B.Input_Protecao",
+    },
+    {
+        "tag": "Program:IHM.EXT_BO_01D_MA_B.Falha_Temperatura",
+        "scan": ".1",
+        "template": bi_template,
+        "name": "IHM:EXT_BO_01D_MA_B.Falha_Temperatura",
+    },
+    {
+        "tag": "Program:IHM.EXT_BO_01D_MA_B.Falha_Protecao",
+        "scan": ".1",
+        "template": bi_template,
+        "name": "IHM:EXT_BO_01D_MA_B.Falha_Protecao",
+    },
+    {
+        "tag": "Program:IHM.EXT_BO_01D_MA_B.Falha_Geral",
+        "scan": ".1",
+        "template": bi_template,
+        "name": "IHM:EXT_BO_01D_MA_B.Falha_Geral",
+    },
+]
 
 if __name__ == "__main__":
-
+    path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "../database/Sirius.db"
+    )
+    with open(path, "w+") as _f:
+        for d in data:
+            _f.write(d["template"].safe_substitute(default, **d))
