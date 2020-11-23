2608ddeb6a4455ad7b43090ba0714aba9b0f7ee6
carneirofc <claudiofcarneiro@hotmail.com>
2020-11-23 14:52:49 -0300

    scrips: Minor fixes

--- scripts/templates.py
+++ scripts/templates.py
@@ -2,17 +2,20 @@
 from string import Template
 
 cmd_template = Template(
-    """#!/opt/epics-R3.15.6/modules/ether_ip-ether_ip-3-1/bin/linux-x86_64/eipIoc
+    """#!../../bin/linux-x86_64/etheripIOC
+< envPaths
+< logEnv
+
+# epicsEnvSet("EPICS_CAS_INTF_ADDR_LIST", "${epics_cas_intf_addr_list}")
+
+cd "${TOP}"
 
 # Load dbd, register the drvEtherIP .. commands
-dbLoadDatabase("/opt/epics-R3.15.6/modules/ether_ip-ether_ip-3-1/dbd/eipIoc.dbd")
-eipIoc_registerRecordDeviceDriver(pdbbase)
+dbLoadDatabase("dbd/etheripIOC.dbd")
+etheripIOC_registerRecordDeviceDriver(pdbbase)
 
-# epicsEnvSet("EPICS_IOC_LOG_INET", "127.0.0.1")
-# epicsEnvSet("EPICS_IOC_LOG_PORT", "6505")
+asSetFilename("${TOP}/db/Security.as")
 
-#epicsEnvSet("EPICS_CA_SERVER_PORT", "${epics_ca_server_port}")
-epicsEnvSet("EPICS_CAS_INTF_ADDR_LIST", "${epics_cas_intf_addr_list}")
 iocLogInit
 
 # Initialize EtherIP driver, define PLCs
@@ -21,10 +24,11 @@ drvEtherIP_init()
 EIP_verbosity(7)
 drvEtherIP_define_PLC("${plc}", "${ip}", ${module})
 
-dbLoadRecords("../database/${database}.db", "PLC=${plc}")
-dbLoadRecords("../database/${database}-Calc.db", "PLC=${plc}")
+dbLoadRecords("database/${database}.db", "PLC=${plc}")
+dbLoadRecords("database/${database}-Calc.db", "PLC=${plc}")
 iocInit()
 
+caPutLogInit "$(EPICS_IOC_CAPUTLOG_INET):$(EPICS_IOC_CAPUTLOG_PORT)" 2
 """
 )
 
