4cae9f41d9656975b9c6e12c53f18fe4b5ee3a90
carneirofc <claudiofcarneiro@hotmail.com>
2020-11-23 15:18:37 -0300

    Fix booster cmd file

--- iocBoot/iocetheripIOC/RF-Booster.cmd
+++ iocBoot/iocetheripIOC/RF-Booster.cmd
@@ -1,14 +1,17 @@
-#!/opt/epics-R3.15.8/modules/ether_ip-ether_ip-3-1/bin/linux-x86_64/eipIoc
+#!../../bin/linux-x86_64/etheripIOC
+< envPaths
+< logEnv
+
+# epicsEnvSet("EPICS_CAS_INTF_ADDR_LIST", "10.128.124.140")
+
+cd "${TOP}"
 
 # Load dbd, register the drvEtherIP .. commands
-dbLoadDatabase("/opt/epics-R3.15.8/modules/ether_ip-ether_ip-3-1/dbd/eipIoc.dbd")
-eipIoc_registerRecordDeviceDriver(pdbbase)
+dbLoadDatabase("dbd/etheripIOC.dbd")
+etheripIOC_registerRecordDeviceDriver(pdbbase)
 
-# epicsEnvSet("EPICS_IOC_LOG_INET", "127.0.0.1")
-# epicsEnvSet("EPICS_IOC_LOG_PORT", "6505")
+asSetFilename("${TOP}/db/Security.as")
 
-#epicsEnvSet("EPICS_CA_SERVER_PORT", "5064")
-epicsEnvSet("EPICS_CAS_INTF_ADDR_LIST", "10.128.124.140")
 iocLogInit
 
 # Initialize EtherIP driver, define PLCs
@@ -17,7 +20,8 @@ drvEtherIP_init()
 EIP_verbosity(7)
 drvEtherIP_define_PLC("plc1", "10.128.130.150", 0)
 
-dbLoadRecords("../database/RF-Booster.db", "PLC=plc1")
-dbLoadRecords("../database/RF-Booster-Calc.db", "PLC=plc1")
+dbLoadRecords("database/RF-Booster.db", "PLC=plc1")
+dbLoadRecords("database/RF-Booster-Calc.db", "PLC=plc1")
 iocInit()
 
+caPutLogInit "$(EPICS_IOC_CAPUTLOG_INET):$(EPICS_IOC_CAPUTLOG_PORT)" 2
