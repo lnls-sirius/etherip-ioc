b8854bfa759a965a017d3694447735fd047f5795
Andrei Pereira <andrei.pereira@lnls.br>
2021-09-10 21:47:04 -0300

    Divide PVs for each module in different db files
    Divide PVs in files depending on the according to undulator
    module, in order to make it easier to choose the module PVs that will run.

--- ioc/iocBoot/iocetheripIOC/Delta_v2.cmd
+++ ioc/iocBoot/iocetheripIOC/Delta_v2.cmd
@@ -26,8 +26,11 @@ drvEtherIP_init()
 EIP_verbosity(7)
 drvEtherIP_define_PLC("plc1", "$(DEVIP)", 0)
 
-dbLoadRecords("database/Delta_v2.db", "PREFIX_MOD_1=${PREFIX_MOD_1}, PREFIX_MOD_2=${PREFIX_MOD_2}, PREFIX_MOD_3=${PREFIX_MOD_3}, PLC=plc1")
-dbLoadRecords("database/Delta_Sabia.db", "P=${PREFIX_MOD_1}, R=")
+dbLoadRecords("database/Delta_Global.db", "PREFIX_GLOBAL=${PREFIX_GLOBAL}, PLC=plc1")
+dbLoadRecords("database/Delta_Mod01.db", "PREFIX_MOD_1=${PREFIX_MOD01}, PLC=plc1")
+#dbLoadRecords("database/Delta_Mod02.db", "PREFIX_MOD_2=${PREFIX_MOD02}, PLC=plc1")
+#dbLoadRecords("database/Delta_Mod02.db", "PREFIX_MOD_3=${PREFIX_MOD03}, PLC=plc1")
+dbLoadRecords("database/Delta_Sabia.db", "P=${PREFIX_MOD01}, R=")
 iocInit()
 
 caPutLogInit "$(EPICS_IOC_CAPUTLOG_INET):$(EPICS_IOC_CAPUTLOG_PORT)" 2
